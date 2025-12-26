8384e4b63c35e8bcc97fee6b9b18fd024d45b00d
Iqbal Hossen <iqbalhossen60483@gmail.com>
2025-12-26 15:00:43 +0600

    mysql env added

--- src/config/config.js
+++ src/config/config.js
@@ -4,10 +4,18 @@ const config = {
   nodeEnv: process.env.NODE_ENV || "development",
   port: process.env.PORT,
   timeZone: process.env.TIME_ZONE || "Asia/Dhaka",
-  tokenSecret: process.env.TOKEN_SECRET || "3c6f85bf510a63c521d554b8daee322e",
+  tokenSecret:
+    process.env.TOKEN_SECRET || "JSDKJFKASDJKFLJSKJAFKLSDJGKJSDKLGJAEIWJFJK",
   redisHost: process.env.REDIS_HOST,
   redisPort: process.env.REDIS_PORT,
   redisPassword: process.env.REDIS_PASS || "",
+  database: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  },
 };
 
 module.exports = { config };
